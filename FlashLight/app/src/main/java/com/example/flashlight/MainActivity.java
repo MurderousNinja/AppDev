fbca58b13c0f3c5bc5e4df4a5beda9ef6b610838
Chinmay Patil <patil.chinmay3031@gmail.com>
2023-10-13 16:19:19 +0530

    Initial commit

---
+++ FlashLight/app/src/main/java/com/example/flashlight/MainActivity.java
@@ -0,0 +1,88 @@
+package com.example.flashlight;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.Manifest;
+import android.widget.Toast;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageButton imageButton;
+    boolean state;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageButton = findViewById(R.id.torchBtn);
+
+        Dexter.withContext(this).withPermission(Manifest.permission.CAMERA).withListener(new PermissionListener() {
+            @Override
+            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                runFlashLight();
+            }
+
+            @Override
+            public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+                Toast.makeText(MainActivity.this,"Camera Permission Required",Toast.LENGTH_SHORT);
+            }
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+
+            }
+        }).check();
+    }
+
+    private void runFlashLight() {
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!state)
+                {
+                    CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+
+                    try{
+                        String cameraID = cameraManager.getCameraIdList()[0];
+                        cameraManager.setTorchMode(cameraID,true);
+                        state = true;
+                        imageButton.setImageResource(R.drawable.flash_on);
+                    }
+                    catch (CameraAccessException e){
+
+                    }
+                }
+                else
+                {
+                    CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+
+                    try{
+                        String cameraID = cameraManager.getCameraIdList()[0];
+                        cameraManager.setTorchMode(cameraID,false);
+                        state = false;
+                        imageButton.setImageResource(R.drawable.flash_off);
+                    }
+                    catch (CameraAccessException e){
+
+                    }
+                }
+            }
+        });
+    }
+}
